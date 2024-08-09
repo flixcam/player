41cf81f462e375b29cb4e069997efe71a618e12e
Sagar Anmol <62541229+sagar2932@users.noreply.github.com>
2024-08-10 03:00:21 +0530

    gtag added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,32 @@
 // pages/_app.js
 
-//import '../public/main.css';
 import '../styles/globals.css';
 import '../styles/main.css';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        id="google-analytics"
+        src="https://www.googletagmanager.com/gtag/js?id=G-R2PVQR1PSL"
+        strategy="afterInteractive"
+      />
+      <Script
+        id="google-analytics-config"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-R2PVQR1PSL');
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
